bb69cd1c83ad61e1b6051d7d2b7e3486844f427c
MiO <mio@synogate.com>
2024-07-08 14:25:32 +0200

    add uart and bitbang operating mode

--- source/find-the-damn-issue.cpp
+++ source/find-the-damn-issue.cpp
@@ -24,29 +24,98 @@ struct FindTheDamnIssue
 	scl::usb::Function usb;
 	scl::usb::SimuBusBase* usbSimuPhy;
 
+	enum class OperatingMode { UART, BitBang };
+	Reg<Enum<OperatingMode>> operatingMode{ OperatingMode::UART };
+
 	Vector<Bit> uio_in_simu;
+	Bit dtr, rts;
+	Bit tx, rx;
 
 	virtual void generate()
 	{
+		operatingMode.setName("operatingMode");
+
+		UInt baudRate = BitWidth::last(hlim::ceil(ClockScope::getClk().absoluteFrequency()));
+
+		enum class SetupClassRequest { none, SET_LINE_CODING };
+		Reg<Enum<SetupClassRequest>> setupClassRequest{ SetupClassRequest::none };
+
+		usb.addClassSetupHandler([&](const scl::usb::SetupPacket& setup) -> Bit {
+			Bit handled = '0';
+			setupClassRequest = SetupClassRequest::none;
+
+			// SET_LINE_CODING
+			IF(setup.request == 0x20 & setup.requestType == 0x21 & setup.wIndex == 0)
+			{
+				setupClassRequest = SetupClassRequest::SET_LINE_CODING;
+				handled = '1';
+			}
+
+			// SET_LINE_CONTROL_STATE
+			IF(setup.request == 0x22 & setup.requestType == 0x21 & setup.wIndex == 0)
+			{
+				dtr = setup.wValue[0];
+				rts = setup.wValue[1];
+				handled = '1';
+			}
+
+			return handled;
+		});
+
+		usb.addClassDataHandler([&](const BVec& packet) {
+			IF(setupClassRequest.current() == SetupClassRequest::SET_LINE_CODING)
+			{
+				baudRate = (UInt)packet.lower(baudRate.width());
+				BVec parity = packet(5 * 8, 8_b);
+				HCL_NAMED(parity);
+
+				operatingMode = OperatingMode::UART;
+				IF(baudRate == 57600 & parity == 2)
+					operatingMode = OperatingMode::BitBang;
+			}
+		});
+
+		dtr = reg(dtr, '0');	HCL_NAMED(dtr);
+		rts = reg(rts, '0');	HCL_NAMED(rts);
+		baudRate = reg(baudRate, 115'200);
+		HCL_NAMED(baudRate);
+
 		generateUsbFunction();
 		scl::BitBangEngine bitbang;
+		bitbang
+			.ioMosi(1)
+			.ioMiso(2)
+			.ioClk(3)
+			.ioTms(4)
+			.ioStopClock(5);
 
-		{
-			scl::TransactionalFifo<scl::usb::Function::StreamData> host2uartFifo{ 16 };
-			usb.attachRxFifo(host2uartFifo, 1 << 1);
-			scl::TransactionalFifo<scl::usb::Function::StreamData> uart2hostFifo{ 16 };
-			usb.attachTxFifo(uart2hostFifo, 1 << 1);
+		// attach usb rx to demux for operating mode dependent forwarding
+		scl::StreamDemux command{ usb.rxEndPointFifo(1, 16), operatingMode.current().numericalValue() };
 
-			scl::RvStream<BVec> command = scl::strm::pop(host2uartFifo)
-				.transform([](const scl::usb::Function::StreamData& in) { return (BVec)in.data; });
+		// uart tx
+		tx = command.out(size_t(OperatingMode::UART)) | scl::uartTx(baudRate);
+		HCL_NAMED(tx);
+			
+		// bitbang engine
+		scl::RvStream<BVec> bitbangOut = bitbang.generate(command.out(size_t(OperatingMode::BitBang)), 16);
+		HCL_NAMED(bitbangOut);
 
-			bitbang.generate(move(command), 16)
-				.transform([](const BVec& d) { return scl::usb::Function::StreamData{ .data = (UInt)d, .endPoint = 1 }; })
-				| scl::strm::push(uart2hostFifo);
+		// uart rx
+		rx.simulationOverride(reg(tx)); // loop back for simple testing
+		HCL_NAMED(rx);
 
-			host2uartFifo.generate();
-			uart2hostFifo.generate();
-		}
+		scl::RvStream<BVec> uartOut = 
+			scl::uartRx(rx, baudRate)
+			.add(scl::Ready{});
+		HCL_NAMED(uartOut);
+		rx = '1';
+
+		// arbitrate and send over usb
+		usb.txEndPointFifo(1, 16, scl::strm::arbitrateWithPolicy(
+			scl::ArbiterPolicyExtern{ operatingMode.current().numericalValue() }, 
+			move(uartOut),
+			move(bitbangOut)
+		));
 
 		pin(bitbang);
 	}
@@ -88,13 +157,21 @@ protected:
 			uio_out[i] = bitbang.io(i).iobufOut();
 			uio_oe[i] = bitbang.io(i).iobufEnable();
 		}
+
+		IF(operatingMode.current() == OperatingMode::UART)
+		{
+			uio_out[1] = tx;
+			uio_oe = 0b0000'0010; // tx out, others in
+		}
+		rx = uio_in[2];
+
 		pinIn(uio_in, "uio_in");
 		pinOut(uio_out, "uio_out");
 		pinOut(uio_oe, "uio_oe");
 		
 		uio_in_simu.resize(8);
 		for (size_t i = 0; i < 6; ++i)
-			uio_in_simu[i] = tristatePin(uio_oe[i], uio_out[i], { .simulationOnlyPin = true });
+			uio_in_simu[i] = tristatePin(uio_out[i], uio_oe[i], { .simulationOnlyPin = true });
 		uio_in.simulationOverride(pack(uio_in_simu));
 	
 		UInt ui_in = 8_b;
@@ -104,6 +181,13 @@ protected:
 			bitbang.io(i + 8).in = ui_in[i];
 			uo_out[i] = bitbang.io(i + 8).out;
 		}
+
+		IF(operatingMode.current() == OperatingMode::UART)
+		{
+			uo_out[0] = dtr;
+			uo_out[1] = rts;
+		}
+
 		pinIn(ui_in, "ui_in");
 		pinOut(uo_out, "uo_out");
 	}
@@ -156,24 +240,69 @@ protected:
 			//co_await controller.testWindowsDeviceDiscovery();
 			co_await controller.controlSetConfiguration(1);
 
-			std::vector<uint8_t> commands = {
-				// spi setup
-				0x80, 0x00, 0x00, 0x82, 0x00, 0x00, 0x9e, 0x00, 0x00, 0x8d, 0x85, 0x86, 0x00, 0x00, 0x80, 0x0b, 0x0b, 0x86, 0x02, 0x00,
-				// spi transfer, send a command byte and receive 8 bytes of data
-				0xc1, 0x13, 0x07, 0xdf, 0xc1, 0x23, 0x3f, 0xc9,
-			};
-			co_await controller.transferOutBatch(1, std::as_bytes(std::span(commands)));
-
-
-			std::vector<std::byte> result;
-			while (result.size() < 8)
+			if (m_simulateUart)
 			{
-				std::vector<std::byte> packet = co_await controller.transferInBatch(1, 64);
-				for (size_t i = 0; i < packet.size(); ++i)
-					HCL_ASSERT((packet[i] == std::byte(0xFF)))
-				result.insert(result.end(), packet.begin(), packet.end());
+				sim::SimulationContext::current()->onDebugMessage(nullptr, "set fast baud rate");
+				std::vector<uint8_t> fastBaudCommand = { 0x80, 0x84, 0x1E, 0x00,  /* stop */ 0x00, /* parity */ 0x00, /* data bits */ 0x08 };
+				co_await controller.controlTransferOut({
+					.direction = scl::usb::EndpointDirection::out,
+					.type = scl::usb::SetupType::class_,
+					.recipient = scl::usb::SetupRecipient::interface,
+					.request = 0x20, //  SET_LINE_CODING
+					.length = uint16_t(fastBaudCommand.size())
+				}, as_bytes(std::span(fastBaudCommand)));
+
+				sim::SimulationContext::current()->onDebugMessage(nullptr, "send uart data");
+				std::vector<uint8_t> uart_data = { 'T' };
+				co_await controller.transferOutBatch(1, as_bytes(std::span(uart_data)));
+
+				sim::SimulationContext::current()->onDebugMessage(nullptr, "set RTS=1, DTR=0 through SET_LINE_CONTROL_STATE");
+				co_await controller.controlTransferOut({
+					.direction = scl::usb::EndpointDirection::out,
+					.type = scl::usb::SetupType::class_,
+					.recipient = scl::usb::SetupRecipient::interface,
+					.request = 0x22, // SET_LINE_CONTROL_STATE
+					.value = 0b10, // RTS=1, DTR=0
+				});
+
+				sim::SimulationContext::current()->onDebugMessage(nullptr, "receive uart data");
+				std::vector<std::byte> uartRxData = co_await controller.transferInBatch(1, 64);
+				HCL_ASSERT(uartRxData.size() == 1);
+				HCL_ASSERT(uartRxData[0] == std::byte('T'));
 			}
-			HCL_ASSERT(result.size() == 8);
+
+			if (m_simulateBitbang)
+			{
+				sim::SimulationContext::current()->onDebugMessage(nullptr, "set operating mode to bitbang");
+				std::vector<uint8_t> opModeBitbangCommand = { 0x00, 0xE1, 0x00, 0x00,  /* stop */ 0x00, /* parity */ 0x02, /* data bits */ 0x08 };
+				co_await controller.controlTransferOut({
+					.direction = scl::usb::EndpointDirection::out,
+					.type = scl::usb::SetupType::class_,
+					.recipient = scl::usb::SetupRecipient::interface,
+					.request = 0x20, //  SET_LINE_CODING
+					.length = uint16_t(opModeBitbangCommand.size())
+				}, as_bytes(std::span(opModeBitbangCommand)));
+
+				sim::SimulationContext::current()->onDebugMessage(nullptr, "spi test");
+				std::vector<uint8_t> commands = {
+					// spi setup
+					0x80, 0b1011, 0b1011, // set pin direction and initial state
+					0x86, 0x02, 0x00, // set clock rate
+					// spi transfer, send a command byte and receive 8 bytes of data
+					0xc8, 0x13, 0x07, 0xdf, 0xc1, 0x23, 0x3f, 0xcb,
+				};
+				co_await controller.transferOutBatch(1, std::as_bytes(std::span(commands)));
+
+				std::vector<std::byte> result;
+				while (result.size() < 8)
+				{
+					std::vector<std::byte> packet = co_await controller.transferInBatch(1, 64);
+					for (size_t i = 0; i < packet.size(); ++i)
+						HCL_ASSERT((packet[i] == std::byte(0xFF)))
+					result.insert(result.end(), packet.begin(), packet.end());
+				}
+				HCL_ASSERT(result.size() == 8);
+			}
 
 			for (size_t i = 0; i < 128; ++i)
 				co_await OnClk(sysclk);
@@ -217,12 +346,14 @@ protected:
 protected:
 	DesignScope design;
 	std::string m_configName = "find-the-damn-issue_deca";
+	bool m_simulateUart = true;
+	bool m_simulateBitbang = true;
 };
 
 int main()
 {
 	FindTheDamnIssueGenerator circuit;
 	circuit.generate();
-    return 0;
+	return 0;
 }
 
