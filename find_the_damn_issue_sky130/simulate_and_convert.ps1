72dd8052151f7850226df0f3d6f8bc9dfe836e46
Leonel Gouveia Ergin <leonel.ge@synogate.com>
2024-07-12 14:27:54 +0200

    added copy file to src folder for templated tt08

--- find_the_damn_issue_sky130/simulate_and_convert.ps1
+++ find_the_damn_issue_sky130/simulate_and_convert.ps1
@@ -1,7 +1,8 @@
-ghdl -a --std=08 -PC:\Users\mio\source\ghdl_libs\intel .\find_the_damn_issue*.vhd
-ghdl -e --std=08 -PC:\Users\mio\source\ghdl_libs\intel -fsynopsys find_the_damn_issue_sky130_tb
-ghdl -r --std=08 -PC:\Users\mio\source\ghdl_libs\intel -fsynopsys find_the_damn_issue_sky130_tb --vcd=ghdl.vcd --ieee-asserts=disable
-ghdl synth --std=08 -PC:\Users\mio\source\ghdl_libs\intel -fsynopsys --out=verilog top | Out-File -FilePath "../src/find_the_damn_issue_sky130.v" -Encoding ascii 
+ghdl -a --std=08 .\find_the_damn_issue*.vhd
+ghdl -e --std=08 -fsynopsys find_the_damn_issue_sky130_tb
+ghdl -r --std=08 -fsynopsys find_the_damn_issue_sky130_tb --vcd=ghdl.vcd --ieee-asserts=disable
+ghdl synth --std=08 -fsynopsys --out=verilog top | Out-File -FilePath "find_the_damn_issue_sky130.v" -Encoding ascii
+cp .\find_the_damn_issue_sky130.v ..\src\
 
 iverilog -o iverilog_test -s find_the_damn_issue_sky130_tb -g2005-sv find_the_damn_issue_sky130_tb.v find_the_damn_issue_sky130.v
 vvp -n iverilog_test
